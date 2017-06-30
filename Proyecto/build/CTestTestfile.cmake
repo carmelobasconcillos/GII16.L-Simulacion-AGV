588a46e7d2bce2fbe286f4ea5feb24e4c15e1258
Carmelo <cbr0016@alu.ubu.es>
2017-06-30 13:15:52 +0200

    Quitamos carpetas no necesarias

--- Proyecto/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/carmelo00/robot/src
-# Build directory: /home/carmelo00/robot/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(gtest)
-subdirs(mirobot)
--- Proyecto/build/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = "/home/carmelo00/robot/src"
-whitelisted_packages = "".split(';') if "" != "" else []
-blacklisted_packages = "".split(';') if "" != "" else []
-underlay_workspaces = "/home/carmelo00/robot/devel;/opt/ros/kinetic".split(';') if "/home/carmelo00/robot/devel;/opt/ros/kinetic" != "" else []
